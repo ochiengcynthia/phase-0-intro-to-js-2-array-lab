be6ecd12fb6c431609be00643f083e9856314e22
ochieng <cindycynthia387@gmail.com>
2023-03-13 02:20:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution here!
+const cats=["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(Otis){
+    cats.push(Otis);
+}   
+function destructivelyPrependCat(Bob){
+    cats.unshift(Bob);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();}
+
+function appendCat(millie){
+    cats.push(millie);}
+
+    function prependCat(Bruno){
+        cats.push(Bruno);}
+
+        function removeLastCat(){
+            cats.slice(3);}
+
+            function removeFirstCat(){
+                cats.slice(1);}
